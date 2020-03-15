56bb9c88d88d3099489376d42b8b64c3b486dae1
Sandu Victor <vyctor661@gmail.com>
2020-03-15 15:19:26 +0200

    added build

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Someone kill me cuz this is the worst way of making a website with ts
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -41,6 +42,13 @@ window.onload = function () {
         var content = (document.getElementById("content"));
         content.innerText = data.error.message;
     };
+    var generateTable = function (data, globalData, country) {
+        return "\n        <table>\n            <tr>\n                <th>Data</th>\n                <th>" + country + "</th>\n                <th>Global</th>\n            </tr>\n            <tr>\n                <td>Confirmed Cases</td>\n                <td>" + data.confirmed.value + "</td>\n                <td>" + globalData.confirmed.value + "</td>\n            </tr>\n            <tr>\n                <td>Confirmed Recoveries</td>\n                <td>" + data.recovered.value + "</td>\n                <td>" + globalData.recovered.value + "</td>\n            </tr>\n            <tr>\n                <td>Confirmed Deaths</td>\n                <td>" + data.deaths.value + "</td>\n                <td>" + globalData.deaths.value + "</td>\n            </tr>\n            <tr>\n                <td>Death Percentage</td>\n                <td>" + ((data.deaths.value / data.confirmed.value) *
+            100).toFixed(2) + "%</td>\n                <td>" + ((globalData.deaths.value / globalData.confirmed.value) *
+            100).toFixed(2) + "%</td>\n            </tr>\n            <tr>\n                <td>Recovery Percentage</td>\n                <td>" + ((data.recovered.value / data.confirmed.value) *
+            100).toFixed(2) + "%</td>\n                <td>" + ((globalData.recovered.value / globalData.confirmed.value) *
+            100).toFixed(2) + "%</td>\n            </tr>\n        </table>";
+    };
     var getData = function (country) { return __awaiter(_this, void 0, void 0, function () {
         var apiData, apiJsonData, error_1;
         return __generator(this, function (_a) {
@@ -62,26 +70,56 @@ window.onload = function () {
             }
         });
     }); };
-    var showData = function () { return __awaiter(_this, void 0, void 0, function () {
-        var country, data, error_2;
+    var getGlobalData = function () { return __awaiter(_this, void 0, void 0, function () {
+        var apiData, apiJsonData, error_2;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    _a.trys.push([0, 2, , 3]);
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, fetch("https://covid19.mathdro.id/api")];
+                case 1:
+                    apiData = _a.sent();
+                    return [4 /*yield*/, apiData.json()];
+                case 2:
+                    apiJsonData = _a.sent();
+                    return [2 /*return*/, apiJsonData];
+                case 3:
+                    error_2 = _a.sent();
+                    console.log(error_2.message);
+                    return [3 /*break*/, 4];
+                case 4: return [2 /*return*/];
+            }
+        });
+    }); };
+    var showData = function () { return __awaiter(_this, void 0, void 0, function () {
+        var country, data, content, _a, _b, _c, error_3;
+        return __generator(this, function (_d) {
+            switch (_d.label) {
+                case 0:
+                    _d.trys.push([0, 5, , 6]);
                     country = (document.getElementById("input")).value;
                     return [4 /*yield*/, getData(country)];
                 case 1:
-                    data = _a.sent();
-                    console.log(data);
-                    if (data.error) {
-                        showError(data);
-                    }
-                    return [3 /*break*/, 3];
+                    data = _d.sent();
+                    if (!data.error) return [3 /*break*/, 2];
+                    showError(data);
+                    return [3 /*break*/, 4];
                 case 2:
-                    error_2 = _a.sent();
-                    console.log(error_2);
-                    return [3 /*break*/, 3];
-                case 3: return [2 /*return*/];
+                    content = (document.getElementById("content"));
+                    _a = content;
+                    _b = generateTable;
+                    _c = [data];
+                    return [4 /*yield*/, getGlobalData()];
+                case 3:
+                    _a.innerHTML = _b.apply(void 0, _c.concat([_d.sent(),
+                        country]));
+                    _d.label = 4;
+                case 4: return [3 /*break*/, 6];
+                case 5:
+                    error_3 = _d.sent();
+                    console.log(error_3);
+                    return [3 /*break*/, 6];
+                case 6: return [2 /*return*/];
             }
         });
     }); };
