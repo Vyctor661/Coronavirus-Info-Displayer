b0e4885db6399d21c60c8083c6042dbff8450642
Matei Adriel <rafaeladriel11@gmail.com>
2020-10-11 18:20:03 +0300

    refactor: now the code is less shitty

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-// Someone kill me cuz this is the worst way of making a website with ts
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -36,93 +39,84 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _this = this;
-window.onload = function () {
-    var button = (document.getElementById("button"));
-    var showError = function (data) {
-        var content = (document.getElementById("content"));
-        content.innerText = data.error.message;
-    };
-    var generateTable = function (data, globalData, country) {
-        return "\n        <table>\n            <tr>\n                <th>Data</th>\n                <th>" + country + "</th>\n                <th>Global</th>\n            </tr>\n            <tr>\n                <td>Confirmed Cases</td>\n                <td>" + data.confirmed.value + "</td>\n                <td>" + globalData.confirmed.value + "</td>\n            </tr>\n            <tr>\n                <td>Confirmed Recoveries</td>\n                <td>" + data.recovered.value + "</td>\n                <td>" + globalData.recovered.value + "</td>\n            </tr>\n            <tr>\n                <td>Confirmed Deaths</td>\n                <td>" + data.deaths.value + "</td>\n                <td>" + globalData.deaths.value + "</td>\n            </tr>\n            <tr>\n                <td>Death Percentage</td>\n                <td>" + ((data.deaths.value / data.confirmed.value) *
-            100).toFixed(2) + "%</td>\n                <td>" + ((globalData.deaths.value / globalData.confirmed.value) *
-            100).toFixed(2) + "%</td>\n            </tr>\n            <tr>\n                <td>Recovery Percentage</td>\n                <td>" + ((data.recovered.value / data.confirmed.value) *
-            100).toFixed(2) + "%</td>\n                <td>" + ((globalData.recovered.value / globalData.confirmed.value) *
-            100).toFixed(2) + "%</td>\n            </tr>\n        </table>";
-    };
-    var getData = function (country) { return __awaiter(_this, void 0, void 0, function () {
-        var apiData, apiJsonData, error_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 3, , 4]);
-                    return [4 /*yield*/, fetch("https://covid19.mathdro.id/api/countries/" + country)];
-                case 1:
-                    apiData = _a.sent();
-                    return [4 /*yield*/, apiData.json()];
-                case 2:
-                    apiJsonData = _a.sent();
-                    return [2 /*return*/, apiJsonData];
-                case 3:
-                    error_1 = _a.sent();
-                    console.log(error_1.message);
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
-            }
-        });
-    }); };
-    var getGlobalData = function () { return __awaiter(_this, void 0, void 0, function () {
-        var apiData, apiJsonData, error_2;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 3, , 4]);
-                    return [4 /*yield*/, fetch("https://covid19.mathdro.id/api")];
-                case 1:
-                    apiData = _a.sent();
-                    return [4 /*yield*/, apiData.json()];
-                case 2:
-                    apiJsonData = _a.sent();
-                    return [2 /*return*/, apiJsonData];
-                case 3:
-                    error_2 = _a.sent();
-                    console.log(error_2.message);
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
-            }
-        });
-    }); };
-    var showData = function () { return __awaiter(_this, void 0, void 0, function () {
-        var country, data, content, _a, _b, _c, error_3;
-        return __generator(this, function (_d) {
-            switch (_d.label) {
-                case 0:
-                    _d.trys.push([0, 5, , 6]);
-                    country = (document.getElementById("input")).value;
-                    return [4 /*yield*/, getData(country)];
-                case 1:
-                    data = _d.sent();
-                    if (!data.error) return [3 /*break*/, 2];
-                    showError(data);
-                    return [3 /*break*/, 4];
-                case 2:
-                    content = (document.getElementById("content"));
-                    _a = content;
-                    _b = generateTable;
-                    _c = [data];
-                    return [4 /*yield*/, getGlobalData()];
-                case 3:
-                    _a.innerHTML = _b.apply(void 0, _c.concat([_d.sent(),
-                        country]));
-                    _d.label = 4;
-                case 4: return [3 /*break*/, 6];
-                case 5:
-                    error_3 = _d.sent();
-                    console.log(error_3);
-                    return [3 /*break*/, 6];
-                case 6: return [2 /*return*/];
-            }
-        });
-    }); };
-    showData();
-    button.addEventListener("click", function () { return showData(); });
+var button = document.getElementById("button");
+var showError = function (data) {
+    var content = document.getElementById("content");
+    content.innerText = data.error.message;
 };
+// This is here so we get syntax highlighting from the lit-html extension
+var html = function (s) {
+    var data = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        data[_i - 1] = arguments[_i];
+    }
+    return s.reduce(function (acc, curr, index) { return "" + acc + data[index - 1] + curr; }, "");
+};
+var calculatePercentage = function (data, key) {
+    return (data[key].value / data.confirmed.value * 100).toFixed(2);
+};
+var generateTable = function (data, globalData, country) {
+    var deathPercentage = {
+        local: calculatePercentage(data, "deaths"),
+        global: calculatePercentage(globalData, "deaths")
+    };
+    var recoveryPercentage = {
+        local: calculatePercentage(data, "recovered"),
+        global: calculatePercentage(globalData, "recovered")
+    };
+    return html(__makeTemplateObject(["\n        <table>\n            <tr>\n                <th>Data</th>\n                <th>", "</th>\n                <th>Global</th>\n            </tr>\n            <tr>\n                <td>Confirmed Cases</td>\n                <td>", "</td>\n                <td>", "</td>\n            </tr>\n            <tr>\n                <td>Confirmed Recoveries</td>\n                <td>", "</td>\n                <td>", "</td>\n            </tr>\n            <tr>\n                <td>Confirmed Deaths</td>\n                <td>", "</td>\n                <td>", "</td>\n            </tr>\n            <tr>\n                <td>Death Percentage</td>\n                <td>", "%</td>\n                <td>", "%</td>\n            </tr>\n            <tr>\n                <td>Recovery Percentage</td>\n                <td>", "%</td>\n                <td>", "%</td>\n            </tr>\n        </table>"], ["\n        <table>\n            <tr>\n                <th>Data</th>\n                <th>", "</th>\n                <th>Global</th>\n            </tr>\n            <tr>\n                <td>Confirmed Cases</td>\n                <td>", "</td>\n                <td>", "</td>\n            </tr>\n            <tr>\n                <td>Confirmed Recoveries</td>\n                <td>", "</td>\n                <td>", "</td>\n            </tr>\n            <tr>\n                <td>Confirmed Deaths</td>\n                <td>", "</td>\n                <td>", "</td>\n            </tr>\n            <tr>\n                <td>Death Percentage</td>\n                <td>", "%</td>\n                <td>", "%</td>\n            </tr>\n            <tr>\n                <td>Recovery Percentage</td>\n                <td>", "%</td>\n                <td>", "%</td>\n            </tr>\n        </table>"]), country, data.confirmed.value, globalData.confirmed.value, data.recovered.value, globalData.recovered.value, data.deaths.value, globalData.deaths.value, deathPercentage.local, deathPercentage.global, recoveryPercentage.local, recoveryPercentage.global);
+};
+var getData = function (country) { return __awaiter(_this, void 0, void 0, function () {
+    var apiData;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, fetch("https://covid19.mathdro.id/api/countries/" + country)];
+            case 1:
+                apiData = _a.sent();
+                return [2 /*return*/, apiData.json()];
+        }
+    });
+}); };
+var getGlobalData = function () { return __awaiter(_this, void 0, void 0, function () {
+    var apiData;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, fetch("https://covid19.mathdro.id/api")];
+            case 1:
+                apiData = _a.sent();
+                return [2 /*return*/, apiData.json()];
+        }
+    });
+}); };
+var showData = function () { return __awaiter(_this, void 0, void 0, function () {
+    var country, data, content, _a, _b, _c, error_1;
+    return __generator(this, function (_d) {
+        switch (_d.label) {
+            case 0:
+                _d.trys.push([0, 3, , 4]);
+                country = (document.getElementById("input")).value;
+                return [4 /*yield*/, getData(country)];
+            case 1:
+                data = _d.sent();
+                if (data.error) {
+                    return [2 /*return*/, showError(data)];
+                }
+                content = (document.getElementById("content"));
+                _a = content;
+                _b = generateTable;
+                _c = [data];
+                return [4 /*yield*/, getGlobalData()];
+            case 2:
+                _a.innerHTML = _b.apply(void 0, _c.concat([_d.sent(),
+                    country]));
+                return [3 /*break*/, 4];
+            case 3:
+                error_1 = _d.sent();
+                console.log(error_1.message || error_1);
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); };
+showData();
+button.addEventListener("click", showData);
